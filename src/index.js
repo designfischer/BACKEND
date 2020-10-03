0fe9063b76f77c99ebef8dbe150afa6add34ba11
Rafael Fischer <47259718+designfischer@users.noreply.github.com>
2020-10-03 11:26:12 -0300

    added port global variable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const connectToMongoDB = require('./database/mongoDbConnection')
 const routes = require('./routes/routes')
 
+const PORT = process.env.LOCAL_PORT
+
 const app = express()
 
 connectToMongoDB()
@@ -11,6 +13,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
-    console.log('Server running')
-})
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
